128a9ff017bf903732a87c37c65eadb51a5c21e2
rsn870 <rsainiranjan@iisc.ac.in>
2023-06-21 12:09:03 +0530

    Adding double datasets

--- grokk_replica/datasets.py
+++ grokk_replica/datasets.py
@@ -78,3 +78,86 @@ class PermutationGroup(AbstractDataset):
 
     def fetch_output(self, a, b):
         return tuple([a[b[i]] for i in range(len(b))])
+    
+
+
+# ------------------------------ DIVIDING LINE --------------------------------
+
+
+class AbstractDataset_Double(abc.ABC):
+    """
+
+    A two operation at a time abstract dataset with the same inputs but  modified fetch_output and fetch_example functions
+
+    Appropriately changes have been made in a copy of train_grokk.py 
+    
+    
+    """
+    def __init__(self, group_elements1: Set, group_elements2: Set, frac_train: float):
+        self.frac_train = frac_train
+        self.group_elements1 = group_elements1
+        self.group_elements2 = group_elements2
+        self.ordered_group_elements1 = list(self.group_elements1)
+        self.ordered_group_elements2 = list(self.group_elements2)
+        self.idx2vocab = ['o', '='] + list(group_elements1.union(group_elements2))
+        self.vocab2idx = {vocab: idx for idx, vocab in enumerate(self.idx2vocab)}
+        self.n_vocab = len(self.idx2vocab)
+        self.n_out = len(group_elements1.union(group_elements2))
+        idxs = list(range(len(self.group_elements1)*len(self.group_elements2)))
+        random.shuffle(idxs)
+        self.train_pairs, self.val_pairs = idxs[:int(len(idxs)*frac_train)], idxs[int(len(idxs)*frac_train):]
+    
+    @abc.abstractmethod
+    def fetch_output(self, a, b):
+        pass
+
+    def encode(self, sequence):
+        return [self.vocab2idx[item] for item in sequence]
+    
+    def decode(self, sequence):
+        return [self.idx2vocab[item] for item in sequence]
+    
+    def form_equation(self, a, b, c):
+        return [a, 'o', b, '=', c]
+    
+    def fetch_example(self, idx):
+        a = self.ordered_group_elements1[idx // len(self.group_elements2)]
+        b = self.ordered_group_elements2[idx % len(self.group_elements2)]
+        c1,c2 = self.fetch_output(a, b)
+        equation1 = self.form_equation(a, b, c1)
+        equation2 = self.form_equation(a, b, c2)
+        enc1 = (self.encode(equation1[:-1]), (self.vocab2idx[c1]-2), equation1)
+        enc2 = (self.encode(equation2[:-1]), (self.vocab2idx[c2]-2), equation2)  #Converting into tuples
+        return enc1,enc2
+    
+    def fetch_train_example(self):
+        idx = random.choice(self.train_pairs)
+        return self.fetch_example(idx)
+
+    def fetch_val_example(self):
+        idx = random.choice(self.val_pairs)
+        return self.fetch_example(idx)
+    
+    class ModSumSubtractDataset(AbstractDataset_Double):
+        def __init__(self, p, frac_train):
+            super(ModSumSubtractDataset, self).__init__(set(range(p)), set(range(p)), frac_train)
+            self.p = p
+        
+        def fetch_output(self, a, b):
+            return ((a + b) % self.p, (a - b) % self.p)
+        
+    class ModSumDivisonDataset(AbstractDataset_Double):
+        def __init__(self, p, frac_train):
+            super(ModSumDivisonDataset, self).__init__(set(range(p)), set(range(1, p)), frac_train)
+            self.p = p
+        
+        def fetch_output(self, a, b):
+            return ((a + b) % self.p, (a * pow(b, self.p-2, self.p)) % self.p)
+        
+    class ModSubtractDivisonDataset(AbstractDataset_Double):
+        def __init__(self, p, frac_train):
+            super(ModSubtractDivisonDataset, self).__init__(set(range(p)), set(range(1, p)), frac_train)
+            self.p = p
+        
+        def fetch_output(self, a, b):
+            return ((a - b) % self.p, (a * pow(b, self.p-2, self.p)) % self.p)
